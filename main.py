d024ee682af4da47330e52a36df6dc680b2377dd
AIOnGraph <aiongraph@gmail.com>
2024-01-12 18:46:06 +0530

    update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from streamlit_tags import st_tags
 from content_updater import crawl_data,content_cleaner_and_content_enhancer,extract_keywords_from_excel
 import time
 import os
-p = os.system('sudo apt-get install libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0  libcups2 libdrm2 libxkbcommon0 libatspi2.0-0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libpango-1.0-0 libcairo2 libasound2        ')
-os.system("sudo playwright install-deps")
+os.system("playwright install-deps")
+# p = os.system('sudo apt-get install libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0  libcups2 libdrm2 libxkbcommon0 libatspi2.0-0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libpango-1.0-0 libcairo2 libasound2        ')
 st.title("Blog Enhancer")
 if "spinner_status" not in st.session_state:
     st.session_state.spinner_status ="Initializing ..."
